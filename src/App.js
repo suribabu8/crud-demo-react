7cc135d56a72b5ff757a942a23c2d8d10195f2a8
Aman Rastogi <aman.rastogi.90281@gmail.com>
2022-06-27 18:29:54 +0530

    basic crud operations in react completed.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Navbar from "./components/layout/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import NotFound from "./components/pages/NotFound";
+import AddUser from "./components/users/AddUser";
+import User from "./components/users/User";
+import EditUser from "./components/users/EditUser";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/users/add" component={AddUser} />
+          <Route exact path="/users/:id" component={User} />
+          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
